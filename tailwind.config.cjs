79799fee371dff2702c34ec1fbf7cbc39cc2dfc8
auoie <105099407+auoie@users.noreply.github.com>
2022-12-12 07:06:58 -0800

    modify font and index.html styling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Public Sans", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        zinc: {
+          950: "#0e0e11",
+        },
+      },
+    },
   },
   plugins: [],
 };
