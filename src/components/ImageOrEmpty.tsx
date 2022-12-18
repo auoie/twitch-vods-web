6f3a6b1b9de803f918b1a1943300dd1b3df5f9aa
auoie <105099407+auoie@users.noreply.github.com>
2022-12-18 08:24:46 -0800

    don't render image if source is empty

--- src/components/ImageOrEmpty.tsx
+++ src/components/ImageOrEmpty.tsx
@@ -3,7 +3,7 @@ import { FC, useState } from "react";
 type Params = Omit<JSX.IntrinsicElements["img"], "onError">;
 export const ImageOrEmpty: FC<Params> = ({ ...params }) => {
   const [bad, setBad] = useState(false);
-  if (bad) {
+  if (params.src === "" || bad) {
     return <div {...params}></div>;
   }
   return (
