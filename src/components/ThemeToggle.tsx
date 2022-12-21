f2e1c4f1b56d7cc9e29410127a27f64e3d69b956
auoie <105099407+auoie@users.noreply.github.com>
2022-12-21 09:39:31 -0800

    simplify theme logic

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,21 +1,32 @@
 import { FC } from "react";
 import { BsSunFill, BsMoonFill } from "react-icons/bs";
-import { useTheme } from "../theme";
+import { atomWithStorage, useUpdateAtom } from "jotai/utils";
 
+type Theme = "light" | "dark";
+const browser = typeof window !== "undefined";
+const themeAtom = atomWithStorage<Theme>(
+  "theme",
+  browser && matchMedia("(prefers-color-scheme: dark)").matches
+    ? "dark"
+    : "light"
+);
 export const ThemeToggle: FC = () => {
-  const [theme, setTheme] = useTheme();
+  const setTheme = useUpdateAtom(themeAtom);
   return (
     <button
       className="h-7 w-7 mr-2 flex items-center justify-center hover:opacity-75"
       onClick={() => {
-        setTheme((prev) => (prev === "dark" ? "light" : "dark"));
+        setTheme((prev) => {
+          const newTheme = prev === "dark" ? "light" : "dark";
+          document.documentElement.classList.remove("light", "dark");
+          document.documentElement.classList.add(newTheme);
+          document.documentElement.style.colorScheme = newTheme;
+          return newTheme;
+        });
       }}
     >
-      {theme === "light" ? (
-        <BsSunFill className="w-full h-full" />
-      ) : (
-        <BsMoonFill className="w-full h-full" />
-      )}
+      <BsSunFill className="dark:hidden w-full h-full" />
+      <BsMoonFill className="hidden dark:block w-full h-full" />
     </button>
   );
 };
